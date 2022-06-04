08171fc8793d522722e746dd32d976d90c81cc79
alina <alina.bregvadze@gmail.com>
2022-06-04 16:19:32 +0400

    add jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 import HeaderView from "./components/layout/HeaderView";
 import TodosComp from "./components/TodosComp";
 import AddTodoComp from "./components/AddTodoComp";
-import { v4 as uuidv4 } from "../node_modules/uuid";
+// import { v4 as uuidv4 } from "../node_modules/uuid";
 export default {
 	name: "app",
 	components: {
@@ -20,37 +20,61 @@ export default {
 	},
 	data() {
 		return {
-			todos: [
-				{
-					id: 1,
-					title: "todo one",
-					completed: true,
-				},
-				{
-					id: 2,
-					title: "todo two",
-					completed: true,
-				},
-				{
-					id: 3,
-					title: "todo three",
-					completed: false,
-				},
-			],
+			todos: [],
 		};
 	},
+
 	methods: {
 		deleteTodo(id) {
-			this.todos = this.todos.filter((todo) => {
-				return todo.id !== id;
-			});
+			// this.todos = this.todos.filter((todo) => {
+			// 	return todo.id !== id;
+			// });
+			fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+				method: "DELETE",
+			})
+				.then((res) => res.json())
+				.then(() => {
+					this.todos = this.todos.filter((todo) => {
+						return todo.id !== id;
+					});
+				})
+				.catch((error) => {
+					console.error("Error:", error);
+				});
 		},
 		addNewTodo(addTodoObj) {
 			const newTodo = addTodoObj;
-			newTodo.id = uuidv4();
+			// newTodo.id = uuidv4();
+			fetch("https://jsonplaceholder.typicode.com/todos", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newTodo),
+			})
+				.then((res) => res.json())
+				.then((newAddedTodo) => {
+					// console.log(newAddedTodo);
 
-			this.todos = [...this.todos, newTodo];
+					this.todos = [...this.todos, newAddedTodo];
+				})
+				.catch((error) => {
+					console.error("Error:", error);
+				});
+
+			// this.todos = [...this.todos, newTodo];
 		},
+	},
+	created() {
+		fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+			.then((res) => res.json())
+			.then((todoList) => {
+				// console.log(todoList);
+				this.todos = todoList;
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
 	},
 };
 </script>
